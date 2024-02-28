f6a8fc845df749f1a9d534094692f129d4fa6d59
askoritan <228nikitalik@gmail.com>
2024-02-29 01:08:13 +0300

    Nikita Shvachko

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,6 +1,18 @@
 
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
-int main(){
+double function_1(double x) {//y= |_frac_{{1};{({x^{5}+x^{4}+1})}}-log({x})
+    return 1 / (1 + (pow(x, 5) + pow(x, 4) + 1)) - log(x) / log(10);
+}
+
+double function_2(double x) {//y=x^{5}+0.1x^{4}+0.4abs({x})-1.2
+    return pow(x, 5) + 0.1 * pow(x, 4) + 0.4 * abs(x) - 1.2;
+}
+
+int main() {
+
+
 }
